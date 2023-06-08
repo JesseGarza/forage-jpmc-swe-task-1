b763ea66110aa450e1fb5f81bdbe1a102f6a3286
JesseGarza <jegarza@gmail.com>
2023-06-08 10:27:52 -0500

    Completed unit test assertions for getDataPoint function.

--- client_test.py
+++ client_test.py
@@ -8,7 +8,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    # Get the price from the first quote
+    price = quotes[0]['top_ask']['price']
 
+    # Perform your calculations
+    calculated_price = price * 0.5  # Example calculation
+
+    expected_price = 60.6  # Example expected value
+    delta = 0.01  # Define the maximum difference allowed between the expected and calculated values
+    self.assertAlmostEqual(calculated_price, expected_price, delta=delta)
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -16,6 +24,12 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    # Get the bid and ask prices from the first quote
+    bid_price = quotes[0]['top_bid']['price']
+    ask_price = quotes[0]['top_ask']['price']
+
+    # Add the assertion
+    self.assertGreater(bid_price, ask_price)
 
   """ ------------ Add more unit tests ------------ """
 
